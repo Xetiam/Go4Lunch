9b28f5e4988ef21237568345a29ae7901b99356d
Xetiam <Xetiam@gmail.com>
2023-01-27 12:17:57 +0100

    *ViewBinding *Authentification :     facebook     google

--- app/src/main/java/com/example/go4lunch/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/go4lunch/ui/login/LoginActivity.java
@@ -1,5 +1,65 @@
 package com.example.go4lunch.ui.login;
 
 
-public class LoginActivity {
+import android.app.LauncherActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.go4lunch.databinding.ActivityLoginBinding;
+import com.firebase.ui.auth.AuthUI;
+import com.example.go4lunch.R;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class LoginActivity extends AppCompatActivity {
+    private LoginViewModel viewModel;
+    private ActivityLoginBinding binding;
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            this::onSignInResult
+    );
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        //TODO: Valider ou invalider la connexion
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        initUI();
+    }
+
+    private void initUI() {
+        List<AuthUI.IdpConfig> fbAuthProvider =
+                Collections.singletonList(new AuthUI.IdpConfig.FacebookBuilder().build());
+        List<AuthUI.IdpConfig> ggAuthProvider =
+                Collections.singletonList(new AuthUI.IdpConfig.GoogleBuilder().build());
+        setListener(ggAuthProvider, binding.googleSigninButton);
+        setListener(fbAuthProvider, binding.fbSigninButton);
+    }
+
+    private void setListener(List<AuthUI.IdpConfig> authProvider, Button signInListener) {
+        signInListener.setOnClickListener(view -> {
+            Intent signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(authProvider)
+                    .build();
+            signInLauncher.launch(signInIntent);
+        });
+    }
 }
--- app/src/main/java/com/example/go4lunch/ui/login/LoginViewModel.java
+++ app/src/main/java/com/example/go4lunch/ui/login/LoginViewModel.java
@@ -1,4 +1,10 @@
 package com.example.go4lunch.ui.login;
 
 public class LoginViewModel {
+    void onFBButtonClicked(){
+
+    }
+    void onGGButtonClicked(){
+
+    }
 }
